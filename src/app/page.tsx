2d85428af00ff8030f5b2a97a325d666cf81bd6b
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-09 10:23:55 -0300

    refact: redo some point on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,19 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Quote } from "@/types/quotes.types";
 
-const apiurl: string = "https://nodejs-quotations-api.vercel.app/quotes/random";
-
 const Home: React.FC = () => {
-  const [quote, setQuotes] = useState<Quote>();
+  const [quote, setQuotes] = useState<Quote| undefined>();
   const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     try {
-      const response = await fetch(apiurl);
-      if (!response.ok) throw new Error("Error on retrieving content");
+      const response = await fetch(
+        "https://nodejs-quotations-api.vercel.app/quotes/random"
+      );
+      console.log(response)
+      if (!response.ok) {
+        throw new Error("Error on retrieving content");
+      }
       const responseToJSON = await response.json();
       setQuotes(responseToJSON);
     } catch (error) {
@@ -32,12 +35,12 @@ const Home: React.FC = () => {
     <div className="h-dvh bg-slate-900 flex items-center justify-center">
       <article className="bg-slate-800 p-8 rounded-xl min-w-[270px] w-[90%] max-w-[650px] min-h-[230px] flex flex-col items-center justify-between relative">
         <h2 className="text-green-300 text-xl text-center font-medium">
-          {`${loading ? "Advice" : `Advice - ${quote?.author}`}`}
+          {`${loading ? "Advice" : `Advice - ${quote!.author}`}`}
         </h2>
         <blockquote
           className={`before:content-['"'] after:content-['"'] leading-7 text-xl text-center font-bold text-blue-200 my-4`}
         >
-          {`${loading ? "Loading..." : quote?.quote}`}
+          {`${loading ? "Loading..." : quote!.quote}`}
         </blockquote>
         <div className="w-full flex items-center gap-2">
           <span className="w-full h-[0.5px] bg-blue-200"></span>
