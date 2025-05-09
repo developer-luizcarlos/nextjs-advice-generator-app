f8e119825fb440f4a3b774ec97debcb492433bf7
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-09 09:23:22 -0300

    feat: create function to get a random quote from the server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,43 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Quote } from "@/types/quotes.types";
+
+const apiurl: string = process.env["NEXT_PUBLIC_API_URL"]!;
 
 const Home: React.FC = () => {
+  const [quote, setQuotes] = useState<Quote>();
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(apiurl);
+      if (!response.ok) throw new Error("Error on retrieving content");
+      const responseToJSON = await response.json();
+      setQuotes(responseToJSON);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const setRandomQuote = () => fetchData();
+
   return (
     <div className="h-dvh bg-slate-900 flex items-center justify-center">
       <article className="bg-slate-800 p-8 rounded-xl min-w-[250px] w-full max-w-[500px] flex flex-col items-center relative">
         <h2 className="text-green-300 text-xl text-center font-medium">
-          Advice
+          {`${loading ? "Advice" : `Advice - ${quote?.author}`}`}
         </h2>
         <blockquote
           className={`before:content-['"'] after:content-['"'] leading-7 text-xl text-center font-bold text-blue-200 my-4`}
         >
-          Lorem ipsum dolor sit amet.
+          {`${loading ? "Loading..." : quote?.quote}`}
         </blockquote>
         <div className="w-full flex items-center gap-2">
           <span className="w-full h-[0.5px] bg-blue-200"></span>
@@ -20,6 +47,7 @@ const Home: React.FC = () => {
         <button
           className="absolute top-[90%] p-3 cursor-pointer bg-green-300 rounded-full transition duration-300 
              hover:shadow-[0_0_20px_hsl(150,100%,66%)]"
+          onClick={setRandomQuote}
         >
           <Image
             src={"images/icon-dice.svg"}
